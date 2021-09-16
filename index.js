f8115976fca9ccbb80d454f691fb55d0c1004fad
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-09-16 16:56:23 +0200

    updated branch
    updated branch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express();
+    const morgan = require('morgan');
+    const app = express();
 
 let myTopTenMovies = [{
     title: 'Movie 1',
@@ -44,12 +45,12 @@ let myTopTenMovies = [{
 
 //GET route ‘movies’ that returns JSON Object
 app.get('/movies', (req, res) => {
- res.json(movies);
+ res.json(myTopTenMovies);
 });
  
 //GET route ‘text’ that returns default textual response
-app.get('/document', (req, res) => {
-res.send('public/document.html', { root: __movie_api });
+app.get('/documentation', (req, res) => {
+res.sendFile('public/documentation.html', { root: __dirname });
 });
 
 // Function for sending static files
@@ -63,5 +64,7 @@ app.use((err, req, res, next) => {
  console.error(err.stack);
  res.status(500).send('Something broke!');
 });
-
+app.listen(8080, () => {
+console.log('Your app is listening on port 8080.');
+});
 
