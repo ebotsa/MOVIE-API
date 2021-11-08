3c854604effca89052ad11803fc9aa8f40177195
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-11-08 22:52:28 +0100

    Update
    Update

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Directors;
-const {
-	check, validationResult
+const {check, validationResult
 } = require('express-validator');
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+	//useNewUrlParser: true,
+	//useUnifiedTopology: true
+//});//
+mongoose.connect(process.env.CONNECTION_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
