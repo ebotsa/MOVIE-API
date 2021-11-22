87225a7c3f4e3ab6b6837fe2ccc97a4f3931ee33
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-11-22 18:46:02 +0100

    Temporal Update
    Temporal Update

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ app.get('/', (req, res) => {
 	res.send('Welcome to MyFlix!');
 });
 //Get a list of all movies
-app.get("/movies", passport.authenticate("jwt", {
-	session: false
-}), (req, res) => {
+app.get("/movies", (req, res) => {
 	Movies.find().then(movies => {
 		res.status(200).json(movies);
 	}).catch(error => {
